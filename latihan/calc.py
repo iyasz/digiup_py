c42fa3b128f658503e88852697c5e5efc995941a
Yasz Avellia <zakamaragames@gmail.com>
2022-12-06 10:50:38 +0700

    test calc py

---
+++ latihan/calc.py
@@ -0,0 +1,14 @@
+# A simple module, calc.py
+
+
+"""add x,y."""
+
+def kali(x, y):
+ return (x * y)
+
+
+"""subtract x,y."""
+
+def pangkat(x, y):
+ return (x ** y)
+
--- latihan/test_calc.py
+++ latihan/test_calc.py
@@ -1,2 +1,9 @@
+#test_calc.py
+
+# importing module calc.py
+
 import calc
+
+print(calc.kali(10, 2))
+print(calc.pangkat(5, 2))
 
