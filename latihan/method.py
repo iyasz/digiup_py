7b385dad83686fcae6170ccdb93218b6acf25332
Yasz Avellia <zakamaragames@gmail.com>
2022-12-06 10:39:22 +0700

    menambah method

---
+++ latihan/method.py
@@ -0,0 +1,11 @@
+# string method
+nama = "yasz avellia"
+print("upper", nama.upper())  # huruf besasr semua
+print("capitalize", nama.capitalize())  # Paling depan yang besar
+print("title", nama.title())  # kata depan yang besar semua
+
+# int
+value = "Harus angka"
+string = "Harus Huruf"
+print("isdigit", value.isdigit())
+print("isaplha", string.isalpha())
