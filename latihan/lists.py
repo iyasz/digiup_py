57cbe36105d432c012267fafeaf91b90cbaa58f0
Yasz Avellia <zakamaragames@gmail.com>
2022-12-06 10:57:43 +0700

    menambah main lists

--- latihan/lists.py
+++ latihan/lists.py
@@ -6,4 +6,20 @@ alpha_list = list((1, 2, 3))
 
 number.append(3)
 
+number.reverse()
+print(number)
+
+number.sort()
+print(number)
+
+number.sort(reverse=True)
+print(number)
+
+#combine lists
+number2 = [1 , 2]
+combo_list = number + number2
+print(combo_list)
+
+combo_list.sort()
+
 print(number)
