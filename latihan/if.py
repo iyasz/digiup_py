63c08464962444b0ba3f5893465c48fc4bb5e997
Yasz Avellia <zakamaragames@gmail.com>
2022-12-06 11:13:52 +0700

    menambah list

---
+++ latihan/if.py
@@ -0,0 +1,17 @@
+nilai = "gerimis"
+#if
+if nilai == "hujan":
+ print("Pakai Jas Hujan")
+#elif = else if
+elif nilai == "gerimis":
+ print("Pakai Payung")
+#jika tidak ada di kondisi diatas
+else:
+ print("Terobos kang")
+
+new_list = [2, 2, 3, 4] 
+x = 1
+if x not in new_list: 
+ print("'x' bukan di list, so this is True!")
+else:
+ print("'x' ada di list !")
